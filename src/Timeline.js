3b1e5f5da6319de715dbc6b375b38eda509a4fd0
DinaTaklit <ez_taklit@esi.dz>
2020-04-15 20:25:24 +0100

    starter code

---
+++ src/Timeline.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './Timeline.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Timeline App
+      </div>
+  
+    );
+  }
+}
+export default App;
+
+
