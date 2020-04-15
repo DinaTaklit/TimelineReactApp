9908a4539029fa65e238f12ed6711de21901b7f1
DinaTaklit <ez_taklit@esi.dz>
2020-04-15 21:46:14 +0100

    feat: Add header component

--- src/Timeline.js
+++ src/Timeline.js
@@ -1,16 +1,44 @@
 import React, { Component } from 'react';
 import './Timeline.css';
 
-class App extends Component {
+
+// @TODO implement Header componenet
+class Header extends React.Component{
   render() {
     return (
-      <div>
-        Timeline App
+      <div className="header">
+        <div className="fa fa-more">
+        </div>
+        <span className="title">Timeline</span>
+
+        <input
+          type="text"
+          className="searchInput"
+          placeholder="Search ..." />
+
+        <div className="fa fa-search searchIcon"></div>
       </div>
-  
     );
   }
 }
+
+// @TODO implement timeline component
+class Content extends React.Component{
+  render(){
+    return (<div></div>);
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="notificationsFrame">
+        <div className="panel">
+          <Header />
+          <Content />
+        </div>
+      </div>
+    )
+  }
+}
 export default App;
-
-
