85134fb6db7a03c6ca02649fb4c5e42d3393f5d6
DinaTaklit <ez_taklit@esi.dz>
2020-04-16 07:32:33 +0100

    feat: upadate contenet componenet with dynamic data

--- src/Timeline.js
+++ src/Timeline.js
@@ -3,6 +3,21 @@ import './bootstrap.min.css';
 import './Timeline.css';
 
 
+// Add a json content 
+const activity = {
+  timestamp: new Date().getTime(),
+  text: "Ate lunch",
+  user: {
+    id: 1,
+    name: 'Nate',
+    avatar: "http://www.croop.cl/UI/twitter/images/doug.jpg"
+  },
+  comments: [
+    { from: 'Ari', text: 'Me too!' }
+  ]
+}
+
+
 // @Done implement Header componenet
 class Header extends React.Component {
   render() {
@@ -32,6 +47,8 @@ class Header extends React.Component {
 // @Done implement timeline component
 class Content extends React.Component {
   render() {
+    const {activity} = this.props; // ES6 destructuring
+    // it is same as writing:  const activity = this.props.activity;
     return (
       <div className="content">
         <div className="line"></div>
@@ -40,17 +57,17 @@ class Content extends React.Component {
         <div className="item">
           <div className="avatar">
             <img 
-            alt='Doug'
-            src="http://www.croop.cl/UI/twitter/images/doug.jpg" />
-            Doug
+            alt= {activity.text}
+            src= {activity.user.avatar} />
+            {activity.user.name}
           </div>
 
           <span className="time">
-            An hour ago
+              {activity.timestamp}
           </span>
           <p>Ate lunch</p>
           <div className="commentCount">
-            2
+            {activity.comments.length}
           </div>
         </div>
 
@@ -70,7 +87,7 @@ class App extends React.Component {
           <Header title="Profile" />
           <Header title="Settings" />
           <Header title="Chat" />
-          <Content />
+          <Content activity={activity}/>
         </div>
       </div>
     )
