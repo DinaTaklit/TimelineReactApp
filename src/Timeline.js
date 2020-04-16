d7946b19ee44e40053df18eb41a754ff2cb1ff14
DinaTaklit <ez_taklit@esi.dz>
2020-04-16 05:28:47 +0100

    feat: Implement content componenet

--- src/Timeline.js
+++ src/Timeline.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
+import './bootstrap.min.css';
 import './Timeline.css';
 
 
-// @TODO implement Header componenet
+// @Done implement Header componenet
 class Header extends React.Component{
   render() {
     return (
       <div className="header">
-        <div className="fa fa-more">
+        <div className="fa fa-more" >
         </div>
         <span className="title">Timeline</span>
 
@@ -22,17 +23,42 @@ class Header extends React.Component{
   }
 }
 
-// @TODO implement timeline component
-class Content extends React.Component{
-  render(){
-    return (<div></div>);
+// @Done implement timeline component
+class Content extends React.Component {
+  render() {
+    return (
+      <div className="content">
+        <div className="line"></div>
+
+      {/* Timeline item */}
+        <div className="item">
+          <div className="avatar">
+            <img 
+            alt='Doug'
+            src="http://www.croop.cl/UI/twitter/images/doug.jpg" />
+            Doug
+          </div>
+
+          <span className="time">
+            An hour ago
+          </span>
+          <p>Ate lunch</p>
+          <div className="commentCount">
+            2
+          </div>
+        </div>
+
+        {/* ... */}
+
+      </div>
+    )
   }
 }
 
 class App extends React.Component {
   render() {
     return (
-      <div className="notificationsFrame">
+      <div className="notificationsFrame container">
         <div className="panel">
           <Header />
           <Content />
